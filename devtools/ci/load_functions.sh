b39d8b5106a3ed3c5158f3e04d17019bb398f056
Hai Nguyen <hainm.comp@gmail.com>
2017-02-20 16:26:49 -0500

    more

--- devtools/ci/load_functions.sh
+++ devtools/ci/load_functions.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
+version=17
+
 function download_ambertools(){
-    version="AmberTools17.20feb17.tar.bz2"
-    url="https://www.dropbox.com/s/t3uudgldmun2lh7/$version?dl=1"
-    wget $url -O $version
-    tar -xf $version
+    tarfile="AmberTools17.20feb17.tar.bz2"
+    url="https://www.dropbox.com/s/t3uudgldmun2lh7/$tarfile?dl=1"
+    wget $url -O $tarfile
+    tar -xf $tarfile
 }
 
-function install_ambertools(){
+function install_ambertools_travis(){
     set -ex
     # This AmberTools version is not an official release. It is meant for testing.
     # DO NOT USE IT PLEASE.
-    cd amber17
+    cd amber$version
     if [ "MINICONDA_WILL_BE_INSTALLED" = "True" ]; then
         yes | ./configure gnu
     elif [ "MINICONDA_IN_AMBERHOME" = "True" ]; then
@@ -30,4 +32,10 @@ function install_ambertools(){
         ./configure --with-python $HOME/miniconda/bin/python gnu
     fi
     make install -j2
+}
+
+function install_ambertools_circleci(){
+    mkdir $HOME/TMP
+    cd $HOME/TMP
+    python $HOME/amber$version/AmberTools/src/conda-recipe/scripts/build_all.py --exclude-osx --py 2.7
 }
