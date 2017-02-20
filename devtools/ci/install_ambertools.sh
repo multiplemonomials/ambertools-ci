c05d7d30140cb2f0d326bb133f1adf326fd7146d
Hai Nguyen <hainm.comp@gmail.com>
2017-02-20 15:29:51 -0500

    link

---
+++ devtools/ci/install_ambertools.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+function download_ambertools(){
+    # This AmberTools version is not an official release. It is meant for testing.
+    # DO NOT USE IT PLEASE.
+    version="AmberTools17.20feb17.tar.bz2"
+    url="https://www.dropbox.com/s/t3uudgldmun2lh7/$version?dl=1"
+    wget $url -O $version
+    tar -xf $version
+    cd amber17
+
+    if [ "MINICONDA_WILL_BE_INSTALLED" = "True" ]; then
+        yes | ./configure gnu
+    elif [ "MINICONDA_IN_AMBERHOME" = "True" ]; then
+        bash AmberTools/src/configure_python --prefix `pwd`
+        ./configure gnu
+    elif [ "USE_WITH_PYTHON" = "True" ]; then
+        bash AmberTools/src/configure_python --prefix $HOME
+        export PATH=$HOME/miniconda/bin:$PATH
+        ./configure --with-python $HOME/miniconda/bin/python gnu
+    elif [ "SKIP_PYTHON" = "True" ]; then
+        ./configure --skip-python gnu
+        source amber.sh # for nab
+    elif [ "PYTHON_VERSION" = "3.5" ]; then
+        bash AmberTools/src/configure_python --prefix $HOME -v $PYTHON_VERSION
+        export PATH=$HOME/miniconda/bin:$PATH
+        ./configure --with-python $HOME/miniconda/bin/python gnu
+    fi
+    make install -j2
+}
