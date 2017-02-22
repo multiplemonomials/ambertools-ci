359de5eca1f229d3aa2f0e6533638dcebf8acbb2
Hai Nguyen <hainm@users.noreply.github.com>
2017-02-22 09:22:41 -0500

    AT17 (#1)
    *  add .travis.yml
    
    * more travis
    
    * link
    
    * remove before_install
    
    * install
    
    * set -ex
    
    * more
    
    * rename
    
    * more
    
    * more
    
    * trigger circleci
    
    * correct path for circleci
    
    * fix travis build [circleci skip]
    
    * update travis [circleci skip]
    
    * update tarfile
    
    * ls
    
    * remove PYTHON_VERSION=2.7 since it's default
    
    * add script: [circleci skip]
    
    * add run test + macos
    
    * add ambermd
    
    * [ci skip]
    
    * [ci skip]
    
    * fix ambertools-test dir
    
    * fix prefix dir after install
    
    * fix travis path
    
    * update README how to test: [ci skip]
    
    * update README how to test: [ci skip]
    
    * [ci skip]
    
    * [ci skip]
    
    * Update README.md
    
    * run_test.sh
    
    * real test
    
    * sanderapi
    
    * fix python 3.5
    
    * use amber.python
    
    * add TEST_LONG
    
    * circleci add sudo
    
    * fix format
    
    * mpi
    
    * fix syntax error
    
    * [ci skip]: skip mac
    
    * use bash ()?
    
    * test ambermini
    
    * circleci: add ambermini build
    
    * turn off test.pbsa: too long?
    
    * fix bash error
    
    * turn off some slow test
    
    * turn off pymsmt test

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
+[![Build Status](https://travis-ci.org/Amber-MD/ambertools-test.svg?branch=at17)](https://travis-ci.org/Amber-MD/ambertools-test)
+[![CircleCI](https://circleci.com/gh/Amber-MD/ambertools-test.svg?style=svg)](https://circleci.com/gh/Amber-MD/ambertools-test)
+
 # ambertools-test
 For testing AmberTools
+
+# How to run your own change?
+1. make your own AmberTools17.{date}.tar.gz file
+```bash
+    cd $AMBERHOME
+    sh ./mkrelease_at # about 5 minutes
+    # then upload to somewhere
+```
+2. update AmberTools17 url in [devtools/ci/load_functions.sh](devtools/ci/load_functions.sh)
+3. update your own test in [run_tests function](devtools/ci/load_functions.sh)
+4. make a pull request to this repo or [activate your travis account](https://travis-ci.org/getting_started)
---
+++ devtools/ci/load_functions.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+
+url="http://ambermd.org/downloads/ambertools-dev/AmberTools17.21feb17.tar.gz"
+tarfile=`python -c "url='$url'; print(url.split('/')[-1])"`
+version=`python -c "tarfile='$tarfile'; print(tarfile.split('.')[0][-2:])"`
+
+function download_ambertools(){
+    wget $url -O $tarfile
+    tar -xf $tarfile
+}
+
+function install_ambertools_travis(){
+    set -ex
+    # This AmberTools version is not an official release. It is meant for testing.
+    # DO NOT USE IT PLEASE.
+    osname=`python -c 'import sys; print(sys.platform)'`
+    cd amber$version
+    if [ $osname = "darwin" ]; then
+        bash AmberTools/src/configure_python --prefix $HOME -v $PYTHON_VERSION
+        export PATH=$HOME/miniconda/bin:$PATH
+        ./configure --with-python $HOME/miniconda/bin/python -macAccelerate clang
+    else
+        if [ "$MINICONDA_WILL_BE_INSTALLED" = "True" ]; then
+            yes | ./configure gnu
+        elif [ "$MINICONDA_IN_AMBERHOME" = "True" ]; then
+            bash AmberTools/src/configure_python --prefix `pwd`
+            ./configure gnu
+        elif [ "$USE_AMBER_PREFIX" = "True" ]; then
+            mkdir $HOME/TMP/
+            yes | ./configure --prefix $HOME/TMP gnu
+        elif [ "$USE_WITH_PYTHON" = "True" ]; then
+            bash AmberTools/src/configure_python --prefix $HOME
+            export PATH=$HOME/miniconda/bin:$PATH
+            ./configure --with-python $HOME/miniconda/bin/python gnu
+        elif [ "$SKIP_PYTHON" = "True" ]; then
+            ./configure --skip-python gnu
+        elif [ "$AMBER_INSTALL_MPI" = "True" ]; then
+            yes | ./configure gnu
+            make install -j2
+            ./configure -mpi gnu # will do make install later
+        elif [ "$PYTHON_VERSION" = "3.5" ]; then
+            bash AmberTools/src/configure_python --prefix $HOME -v 3
+            export PATH=$HOME/miniconda/bin:$PATH
+            ./configure --with-python $HOME/miniconda/bin/python gnu
+        fi
+    fi
+    make install -j2
+}
+
+function install_ambertools_circleci(){
+    mkdir $HOME/TMP
+    cd $HOME/TMP
+    python $HOME/ambertools-test/amber$version/AmberTools/src/conda-recipe/scripts/build_all.py --exclude-osx --py 2.7 --sudo
+    python $HOME/ambertools-test/amber$version/AmberTools/src/conda-recipe/scripts/build_all.py --exclude-osx --py 2.7 --sudo -t ambermini
+}
+
+function run_long_test_simplified(){
+    # not running all tests, skip any long long test.
+    (cd $AMBERHOME/test/sanderapi && make)
+    cd $AMBERHOME/AmberTools/test
+
+    make clean
+    make is_amberhome_defined
+    make test.cpptraj
+    make test.pytraj
+    make test.parmed
+    make test.pdb4amber
+    make test.leap
+    make test.unitcell
+    make test.antechamber
+    make test.nab
+    make test.mdgx
+    make test.resp
+    make test.reduce
+    # make test.pbsa
+    make test.gbnsr6
+    # make test.mmpbsa
+    make test.elsize
+    make test.sqm
+    make test.paramfit
+    # make test.mm_pbsa
+    make test.FEW
+    make test.cphstats
+    # make test.charmmlipid2amber
+    make test.cpinutil
+    # make test.pymsmt
+}
+
+function run_tests(){
+    set -ex
+    if [ "$USE_AMBER_PREFIX" = "True" ]; then
+        source $HOME/TMP/amber.sh
+    else
+        source $TRAVIS_BUILD_DIR/amber$version/amber.sh
+    fi
+    if [ "$TEST_LONG" = "True" ]; then
+        run_long_test_simplified
+    else
+        if [ "$SKIP_PYTHON" != "True" ]; then
+            cat $TRAVIS_BUILD_DIR/amber$version/AmberTools/src/conda-recipe/run_test.sh | sed "s/python/amber.python/g" > $HOME/run_test.sh
+            bash $HOME/run_test.sh
+        else
+            (cd $AMBERHOME/AmberTools/test && make test.ambermini)
+        fi
+    fi
+}
