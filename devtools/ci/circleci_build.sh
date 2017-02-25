ee811547838ba742a56ab9b1ba9fee251444f74f
Hai Nguyen <hainm.comp@gmail.com>
2017-02-25 02:29:44 -0500

    update url + usig amber.python

--- devtools/ci/circleci_build.sh
+++ devtools/ci/circleci_build.sh
@@ -8,7 +8,7 @@ amber_source=$HOME/ambertools-test/amber17
 
 # binary
 fn='AT.binary.tar.bz2'
-url='https://90-81537431-gh.circle-artifacts.com/0/tmp/circle-artifacts.mHmoQJ3/ambertools-build/amber-conda-bld/non-conda-install/linux-64.ambertools-17.0.0-py27_0.25Feb17.H0203.tar.bz2'
+url='https://96-81537431-gh.circle-artifacts.com/0/tmp/circle-artifacts.bQx72o3/ambertools-build/amber-conda-bld/non-conda-install/linux-64.ambertools-17.0.0-py27_0.25Feb17.H0711.tar.bz2'
 wget $url -O $fn
 mkdir TEST
 cd TEST
@@ -24,9 +24,12 @@ cd $AMBERHOME/AmberTools/test
 lndir $amber_source/AmberTools/test >& log.lndirt2
 
 cd $AMBERHOME
+sh bin/configure_python --prefix $AMBERHOME
+which amber.python
 cat > config.h <<EOF
 INSTALLTYPE=serial
 AMBER_SOURCE=$amber_source
+PYTHON=amber.python
 EOF
 
 mkdir -p $AMBERHOME/AmberTools/src
