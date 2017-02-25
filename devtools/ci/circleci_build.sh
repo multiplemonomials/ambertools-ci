220eb9f2e8e311e67eaeec33d53985ed3db1eae0
Hai Nguyen <hainm.comp@gmail.com>
2017-02-24 23:21:47 -0500

    test binary

--- devtools/ci/circleci_build.sh
+++ devtools/ci/circleci_build.sh
@@ -1,5 +1,26 @@
 #!/bin/sh
 
 source devtools/ci/load_functions.sh
+# source code
 download_ambertools
-install_ambertools_circleci
+
+# binary
+fn='AT.binary.tar.bz2'
+url='https://circleci.com/api/v1.1/project/github/Amber-MD/ambertools-test/latest/artifacts?circle-token=$ATCITOKEN&branch=nightly&filter=successful'
+wget $url -O $fn
+mkdir TEST
+cd TEST
+tar -xf ../$fn
+cd amber17
+source amber.sh
+mkdir test
+cd test
+lndir $HOME/amber17/test
+cd $AMBERHOME/AmberTools
+mkdir test
+cd test
+lndir $HOME/AmberTools/test
+cd $AMBERHOME
+touch config.h
+touch $AMBERHOME/AmberTools/src/config.h
+INSTALLTYPE make test
