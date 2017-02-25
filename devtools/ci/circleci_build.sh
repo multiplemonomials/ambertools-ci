ae371d5a7a9123d2e988bbc3b03edd2f4d12e7bc
Hai Nguyen <hainm.comp@gmail.com>
2017-02-24 23:54:14 -0500

    amber_source

--- devtools/ci/circleci_build.sh
+++ devtools/ci/circleci_build.sh
@@ -4,6 +4,8 @@ source devtools/ci/load_functions.sh
 # source code
 download_ambertools
 
+amber_source=$HOME/ambertools-test/amber17
+
 # binary
 fn='AT.binary.tar.bz2'
 url='https://90-81537431-gh.circle-artifacts.com/0/tmp/circle-artifacts.mHmoQJ3/ambertools-build/amber-conda-bld/non-conda-install/linux-64.ambertools-17.0.0-py27_0.25Feb17.H0203.tar.bz2'
@@ -15,11 +17,11 @@ cd amber17
 source amber.sh
 mkdir test
 cd test
-lndir $HOME/amber17/test
+lndir $amber_source/test
 cd $AMBERHOME/AmberTools
 mkdir test
 cd test
-lndir $HOME/AmberTools/test
+lndir $amber_source/AmberTools/test
 cd $AMBERHOME
 touch config.h
 touch $AMBERHOME/AmberTools/src/config.h
