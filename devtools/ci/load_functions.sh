4115d941252b8354a2b343e6b7bedc96d642b8fa
HN <nhai.qn@gmail.com>
2017-03-02 08:19:39 -0500

    correct

--- devtools/ci/load_functions.sh
+++ devtools/ci/load_functions.sh
@@ -64,7 +64,9 @@ function circleci_test(){
     # install conda
     bash $HOME/ambertools-test/amber$version/AmberTools/src/configure_python --prefix $HOME
     export PATH=$HOME/miniconda/bin:$PATH
-    python $HOME/ambertools-test/amber$version/AmberTools/src/conda_tools/test_multiple_pythons.py
+    for tarfile in `ls $HOME/TMP/amber-conda-bld/linux-64/ambertools-*.tar.bz2`; do
+        python $HOME/ambertools-test/amber$version/AmberTools/src/conda_tools/test_multiple_pythons.py $tarfile
+    done
 }
 
 function run_tests(){
