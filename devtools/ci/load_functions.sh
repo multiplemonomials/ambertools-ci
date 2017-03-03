e586fef0d5c34e95ef95c3ccec852fc13bf70085
HN <nhai.qn@gmail.com>
2017-03-02 23:24:21 -0500

    simplify

--- devtools/ci/load_functions.sh
+++ devtools/ci/load_functions.sh
@@ -50,8 +50,7 @@ function install_ambertools_travis(){
 function install_ambertools_circleci(){
     mkdir $HOME/TMP
     cd $HOME/TMP
-    python $HOME/ambertools-test/amber$version/AmberTools/src/conda_tools/build_all.py --exclude-osx -t ambertools_pack_all_pythons --sudo
-    # python $HOME/ambertools-test/amber$version/AmberTools/src/conda-recipe/scripts/build_all.py --exclude-osx --py 2.7 --sudo -t ambermini
+    python $HOME/ambertools-test/amber$version/AmberTools/src/conda_tools/build_all.py --exclude-osx --sudo
 }
 
 function run_long_test_simplified(){
