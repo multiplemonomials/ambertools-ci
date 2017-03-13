4ef228431c0077b72549ac532412a1670240fe7c
Hai Nguyen <hainm@users.noreply.github.com>
2017-03-13 00:30:46 -0400

    add circleci test (#5)
    * try
    
    * bash
    
    * more
    
    * more
    
    * more

---
+++ devtools/ci/circleci_test.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+version=17
+function circleci_test(){
+    # install conda
+    bash $HOME/ambertools-test/amber$version/AmberTools/src/configure_python --prefix $HOME
+    export PATH=$HOME/miniconda/bin:$PATH
+    for tarfile in `ls $HOME/TMP/amber-conda-bld/linux-64/ambertools-*.tar.bz2`; do
+        python $HOME/ambertools-test/amber$version/AmberTools/src/conda_tools/test_multiple_pythons.py $tarfile
+    done
+}
+
+circleci_test
--- devtools/ci/load_functions.sh
+++ devtools/ci/load_functions.sh
@@ -65,15 +65,6 @@ function run_long_test_simplified(){
     python $TRAVIS_BUILD_DIR/amber$version/AmberTools/src/conda_tools/amber.run_tests
 }
 
-function circleci_test(){
-    # install conda
-    bash $HOME/ambertools-test/amber$version/AmberTools/src/configure_python --prefix $HOME
-    export PATH=$HOME/miniconda/bin:$PATH
-    for tarfile in `ls $HOME/TMP/amber-conda-bld/linux-64/ambertools-*.tar.bz2`; do
-        python $HOME/ambertools-test/amber$version/AmberTools/src/conda_tools/test_multiple_pythons.py $tarfile
-    done
-}
-
 function run_tests(){
     set -ex
     if [ "$USE_AMBER_PREFIX" = "True" ]; then
